f64e7a660b6cb8fd9fb7576ab47d996a32795a19
Quyen Pham Khac <pkquyen@sk-global.biz>
2020-09-14 01:03:44 +0700

    untyped constants

--- day_08_constants/main.go
+++ day_08_constants/main.go
@@ -31,6 +31,21 @@ const (
 	Saturday
 )
 
+// MyFloat64 type
+type MyFloat64 float64
+
+// Zero constants
+const Zero = 0.0
+
+// TypedZero constants
+const TypedZero float64 = 0.0
+
+// Huge constants
+const Huge = 1e1000
+
+// MaxUint constants
+const MaxUint = ^0
+
 // Constants func
 func Constants() {
 	const noDelay time.Duration = 0
@@ -39,8 +54,28 @@ func Constants() {
 	fmt.Printf("%T %[1]v\n", timeout)
 	fmt.Printf("%T %[1]v\n", time.Minute)
 	fmt.Printf("%T %[1]v\n", Sunday)
-	var x float32 = math.Pi
-	var y float64 = math.Pi
-	var z complex128 = math.Pi
-	fmt.Println(x, y, z)
+	var mf MyFloat64
+	mf = 0.0
+	mf = Zero
+	fmt.Printf("%f\n", mf)
+	fmt.Println(Huge / 1e999)
+	pi := math.Pi
+	var pi32 float32 = math.Pi
+	fmt.Println(pi, pi32)
+	fmt.Println(MaxUint)
+	var f float32 = 1
+	var i int = 1.000
+	var u uint32 = 1e3 - 99.0*10.0 - 9
+	var c float64 = '\x01'
+	var p uintptr = '\u0001'
+	var r complex64 = 'b' - 'a'
+	var b byte = 1.0 + 3i - 3.0i
+
+	fmt.Println(f, i, u, c, p, r, b)
+
+	var f1 = 'a' * 1.5
+	fmt.Println(f1)
+	// var i1 int = 2
+	sqrt2 := math.Sqrt(2.0)
+	fmt.Println(sqrt2)
 }
