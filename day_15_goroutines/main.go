b73495d63df480a0012f224b26b9577d85104ef7
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-03-10 15:43:49 +0700

    feat: resize image func

---
+++ day_15_goroutines/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"go-learning/day_15_goroutines/thumbnail"
+	"log"
+	"os"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		thumb, err := thumbnail.ImageFile(input.Text())
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Println(thumb)
+	}
+	if err := input.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
