7920fe28dc7e45d3ab25be333db4b1542213769b
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-03-10 10:35:17 +0700

    feat: clock server simulating

--- day_08_strings_3/main.go
+++ day_08_strings_3/main.go
@@ -19,9 +19,6 @@ func Strings() {
 	r := []rune(s1)
 	fmt.Printf("%x\n", r)
 	fmt.Println(string(r))
-	fmt.Println(string(65))
-	fmt.Println(string(0x4eac))
-	fmt.Println(string(1234567))
 }
 
 // StringsByteSlices func
---
+++ day_15_goroutines/go.mod
@@ -0,0 +1,3 @@
+module go-learning/day_15_goroutines
+
+go 1.14
