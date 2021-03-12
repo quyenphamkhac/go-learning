881a88f8929248d78cc45f03034216d69642c2d0
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-03-12 10:45:34 +0700

    feat: randomize testing

--- day_17_testing/word/word_test.go
+++ day_17_testing/word/word_test.go
@@ -1,6 +1,10 @@
 package word
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestPalindrome(t *testing.T) {
 	input1 := "detartrated"
@@ -55,6 +59,29 @@ func TestIsPalindrome(t *testing.T) {
 	for _, test := range tests {
 		if got := IsPalindrome(test.input); got != test.want {
 			t.Errorf("IsPalindrome(%q) = %v, want %v", test.input, got, test.want)
+		}
+	}
+}
+
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25)
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000))
+		runes[i] = r
+		runes[n-i-1] = r
+	}
+	return string(runes)
+}
+
+func TestRandomPalindromes(t *testing.T) {
+	seed := time.Now().UnixNano()
+	t.Logf("Randome seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false", p)
 		}
 	}
 }
