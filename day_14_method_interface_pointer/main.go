235c98258e3d835ae4e529718d53050838b2ab2d
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-03-16 09:40:32 +0700

    change: remove warning

--- day_14_method_interface_pointer/main.go
+++ day_14_method_interface_pointer/main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 // Point ...
 type Point struct {
 	X int
@@ -14,14 +10,12 @@ type Point struct {
 func (p Point) Scale(n int) {
 	p.X = p.X * n
 	p.Y = p.Y * n
-	return
 }
 
 // Scale1 ...
 func (p *Point) Scale1(n int) {
 	p.X = p.X * n
 	p.Y = p.Y * n
-	return
 }
 
 // ValueReceiver ...
@@ -37,11 +31,5 @@ func PointerReceiver(p *Point, ratio int) {
 	p.Y = p.Y * ratio
 }
 
-func example(n string, handlers ...int) {
-	fmt.Println(n, handlers)
-}
-
 func main() {
-	{
-	}
 }
