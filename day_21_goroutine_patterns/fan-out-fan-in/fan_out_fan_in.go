64d3a55bdaa8d38e8d2e7ddae12e89f8e56ab0f1
Quyen Pham Khac <pkquyen@sk-global.biz>
2021-03-19 15:34:53 +0700

    feat: fanin fanout

---
+++ day_21_goroutine_patterns/fan-out-fan-in/fan_out_fan_in.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	in := gen(2, 3)
+	c1 := sq(in)
+	c2 := sq(in)
+	for n := range merge(c1, c2) {
+		fmt.Println(n)
+	}
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
